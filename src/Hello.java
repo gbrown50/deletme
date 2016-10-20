6ea221ba523b7b8defbba4a1ed4f0a881293886a
Gary <gbrown50@hotmail.com>
2016-10-20 10:47:17 +0100

    no intrestamount calculation
    added code to calculate the intrest for a given
    -principal
    -intrest amount
    -number of years

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,13 @@ public class Hello {
     public static void main(String[] args) {
 
           System.out.println("Hello World");
+        int numYears = 10;
+        float principal = 10000.00f;
+        float intrestRate = 5.0f;
+
+        float amount = (float)(principal * Math.pow((1 + (intrestRate /100.0f)),numYears));
+
+        System.out.println(amount);
         }
-    }
+        }
 
